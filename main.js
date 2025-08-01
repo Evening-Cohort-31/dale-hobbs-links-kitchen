17e036205147ce116c01769940fe5f8ceeba3a1a
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-08-01 12:47:54 -0500

    modifying my code some more

--- main.js
+++ main.js
@@ -13,30 +13,47 @@ const recipeBook = {
 }
 
 //function that standardizes the input of user values
+//.trim() is a string method removing whitespace from either end
+//.toLowerCase() is a string method making everything lowercase
+//.replace(...) is a string method 
+// this part "/\b\w/g" is a regular expression that finds the first letter of each word entered
+// \b word boundary
+// \w matches any word character (letter, digit, underscore)
+// /g is the global flag so it matches EVERY word in the string
+// char => char.toUpperCase() taking each matched first letter and capitalizes it
 const standardize = (ingredient) => {
     return ingredient.trim().toLowerCase().replace(/\b\w/g, char => char.toUpperCase())
 }
 
 //preprocess materials into a Set for faster lookups
-const standardizedMaterials = new Set(materials.map(normalize))
+//A Set is a collection of unique values and can use the .has() method
+//materials.map(...) creates a new array wehre each item in materials has been cleaned up using standardize() function.
+//new Set(...) constructs a new Set object
+const standardizedMaterials = new Set(materials.map(standardize))
 
 //function that cooks two ingredients
 const cook = (firstIngredient, secondIngredient) => {
+    //standardize user entries
     const standardFirst = standardize(firstIngredient);
     const standardSecond = standardize(secondIngredient);
 
+    //Check for if the ingredients entered match ingredients used to make meals in the recipeBook
     const invalidIngredients = [];
     if (!standardizedMaterials.has(standardFirst)) invalidIngredients.push(firstIngredient);
     if (!standardizedMaterials.has(standardSecond)) invalidIngredients.push(secondIngredient);
 
+    //Conditions to exit cook function if an invalid ingredient is used as an argument
     if (invalidIngredients.length > 0) {
         console.warn(`${invalidIngredients.join(' and ')} ${invalidIngredients.length === 1 ? 'is' : 'are'} not valid cooking material${invalidIngredients.length > 1 ? 's' : ''}.`)
         return
     }
 
+    //  
     const ingredientsKey = [standardFirst, standardSecond].sort().join(',')
     const meal = recipeBook[ingredientsKey]
 
+    //logic for returning the meal if there is a known meal from the two ingredients used as an argument
+    //includes logic to warn user if the ingredients do not make a known meal
     if (meal) {
         meals.push(meal)
         console.log(`You made: ${meal}`)
@@ -52,6 +69,3 @@ console.log('---------------')
 for (const meal of meals) {
     console.log(meel)
 }
-
-//.map workshop notes
-//
